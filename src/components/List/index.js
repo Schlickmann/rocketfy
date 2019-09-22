bec1dd2a30954d73ddff7ba873d77efeb0815256
Juliani Schlickmann Damasceno <julianischlickmanndamasceno@MacBook-Pro.local>
2019-09-22 13:13:00 -0700

    added drag and drop

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useContext, useRef } from "react";
 import { MdAdd } from "react-icons/md";
+import { useDrop } from "react-dnd";
 
+import BoardContext from "../Board/context";
 import Card from "../Card";
 
 import { Container } from "./styles";
 
-export default function List({ data }) {
+export default function List({ data, index: listIndex }) {
+  const ref = useRef();
+  const { move } = useContext(BoardContext);
+  const [, dropRef] = useDrop({
+    accept: "CARD",
+    hover(item, monitor) {
+      const draggedListIndex = item.listIndex;
+      const targetListIndex = listIndex;
+      const draggedIndex = item.index;
+      const targetIndex = data.cards.length !== 0 ? data.cards.length - 1 : 0;
+
+      console.log(targetIndex);
+
+      if (draggedListIndex === targetListIndex) return;
+      move(draggedListIndex, targetListIndex, draggedIndex, targetIndex);
+
+      item.index = targetIndex;
+      item.listIndex = targetListIndex;
+    }
+  });
+  dropRef(ref);
   return (
-    <Container done={data.done}>
+    <Container ref={ref} done={data.done}>
       <header>
         <h2>{data.title}</h2>
         {data.creatable && (
@@ -18,8 +40,8 @@ export default function List({ data }) {
       </header>
 
       <ul>
-        {data.cards.map(card => (
-          <Card key={card.id} data={card} />
+        {data.cards.map((card, index) => (
+          <Card key={card.id} listIndex={listIndex} index={index} data={card} />
         ))}
       </ul>
     </Container>
